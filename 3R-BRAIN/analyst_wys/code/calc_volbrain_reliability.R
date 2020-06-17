9d3c962405cb07902152046e8364a46d29d6329f
Xi-Nian Zuo <zuoxinian@users.noreply.github.com>
2020-06-17 10:36:47 +0800

    Add files via upload

---
+++ 3R-BRAIN/analyst_wys/code/calc_volbrain_reliability.R
@@ -0,0 +1,293 @@
+################################################################################################################################
+# The between session, between scanner and total reliability analysis
+# Between session reliability is defined as the ratio of between participants variance to variance caused by different sessions
+# Between scanner reliability is defined as the ratio of between participants variance to variance caused by different scanners
+# Total reliability is defined as the ratio of between participants variance to variance we observed
+# sigma_subid square is the variance between participants
+# sigma_scanner square is the variance between scanners
+# sigma_session square is the variance between sessions
+# sigma_scanner_session square is the variance caused by different sessions and different scanner
+# sigma_subid_session square is the variance by inter individual differences differ by sessions
+# sigma_subid_scanner square is the variance by inter individual differences differ by scanners
+# Asymmetry Index(AI) is defined as ((LeftHemi - RightHemi)/(LeftHemi + RightHemi))*2
+###############################################################################################################################
+rm(list=ls())
+#load packages
+library("lmerTest")           
+library("boot")
+
+#set path
+workdir <- '/Home/R3BRAIN/'      #Should change it to your path accordingly
+setwd(workdir)
+
+#load data file
+fname <- paste0(workdir,'/data/','volbrain_reliability.csv')
+volbrain_df <- read.csv(fname,header = TRUE)
+
+#define bootstrap function to compute 3 different iccs
+##########################################functions###########################################################################
+calc.icc_session <- function(fm){
+  results <- summary(fm)
+  varcor <- as.data.frame(results$varcor)                   
+  #variance of each factor
+  sigma_subid <- varcor[3,4]
+  sigma_scanner <- varcor[5,4]
+  sigma_session <- varcor[6,4]
+  sigma_scanner_session <-varcor[4,4]
+  sigma_subid_scanner <- varcor[1,4]
+  sigma_subid_session <- varcor[2,4]
+  sigma_error <- varcor[7,4]
+  sigma_all <- sum(varcor$vcov)
+  vary <- var(tmpdf$y)
+  sigma_subid/(sigma_subid+sigma_session+sigma_subid_session+sigma_error)
+}
+calc.icc_scanner <- function(fm){
+  results <- summary(fm)
+  varcor <- as.data.frame(results$varcor)                   
+  #variance of each factor
+  sigma_subid <- varcor[3,4]
+  sigma_scanner <- varcor[5,4]
+  sigma_session <- varcor[6,4]
+  sigma_scanner_session <-varcor[4,4]
+  sigma_subid_scanner <- varcor[1,4]
+  sigma_subid_session <- varcor[2,4]
+  sigma_error <- varcor[7,4]
+  sigma_all <- sum(varcor$vcov)
+  vary <- var(tmpdf$y)
+  sigma_subid/(sigma_subid+sigma_scanner+sigma_subid_scanner+sigma_error)
+}
+
+calc.icc <- function(fm){
+  results <- summary(fm)
+  varcor <- as.data.frame(results$varcor) 
+  sigma_subid <- varcor[3,4]
+  sigma_scanner <- varcor[5,4]
+  sigma_session <- varcor[6,4]
+  sigma_scanner_session <-varcor[4,4]
+  sigma_subid_scanner <- varcor[1,4]
+  sigma_subid_session <- varcor[2,4]
+  sigma_error <- varcor[7,4]
+  sigma_all <- sum(varcor$vcov)
+  vary <- var(tmpdf$y)
+  icc <- sigma_subid/sigma_all
+}
+
+
+############################################ calculate icc that gained by volbrain ############################################
+
+#centralize data
+center_cols_names <- c("interval_baseline","age","height","weight","education")
+chosen_cols <- volbrain_df[,center_cols_names]
+centered_values <- scale(chosen_cols,center = TRUE)
+centered_cols_names <- c("cinterval_baseline","cage","cheight","cweight","ceducation")
+volbrain_df[,centered_cols_names] <- centered_values
+volbrain_df$sex <- as.factor(volbrain_df$sex)
+
+#create results dataframe
+metric_names <- names(volbrain_df)[18:77]
+nmetrics <- length(metric_names)
+rownames <- c('icc_session','icc_scanner','icc','vary','var_subid','var_scanner','var_session','var_scanner_session','var_subid_scanner','var_subid_session','varw')
+nrows <- length(rownames)
+iccs <- data.frame(matrix(0,nrows,nmetrics))
+names(iccs) <- metric_names
+row.names(iccs) <- rownames
+p_values <- data.frame(matrix(0,2,nmetrics))
+names(p_values) <- metric_names
+row.names(p_values) <- c('Intercept','pvalue')
+ci_2.5 <- data.frame(matrix(0,3,nmetrics))
+names(ci_2.5) <- metric_names
+row.names(ci_2.5) <- c('icc_session','icc_scanner','icc')
+ci_97.5 <- data.frame(matrix(0,3,nmetrics))
+names(ci_97.5) <- metric_names
+row.names(ci_97.5) <- c('icc_session','icc_scanner','icc')
+
+#fit the 3-level lme model
+for (i in 1:nmetrics){
+  tmpdf <- volbrain_df
+  print(metric_names[i])
+  metric <- metric_names[i]
+  tmpdf$y <- tmpdf[[metric]]
+  index_outliers <- !tmpdf$y %in% boxplot.stats(tmpdf$y)$out  #exclude outliers
+  tmpdf <- tmpdf[index_outliers,]
+  try({
+  #use lmer function fit linear mixed effect model
+  fm <- lmer(y ~cage + sex + cheight + cweight + ceducation + cinterval_baseline + (1|subid)+(1|session)+(1|scanner)+(1|subid:session)+(1|subid:scanner)+(1|session:scanner),data=tmpdf)
+  results <- summary(fm)
+  varcor <- as.data.frame(results$varcor)
+  p_value <- results$coefficients[1,c(1,5)]
+  #variance of each factor
+  sigma_subid <- varcor[3,4]
+  sigma_scanner <- varcor[5,4]
+  sigma_session <- varcor[6,4]
+  sigma_scanner_session <-varcor[4,4]
+  sigma_subid_scanner <- varcor[1,4]
+  sigma_subid_session <- varcor[2,4]
+  sigma_error <- varcor[7,4]
+  sigma_all <- sum(varcor$vcov)
+  vary <- var(tmpdf$y)
+  
+  var_subid <- sigma_subid/vary  
+  var_scanner <- sigma_scanner/vary
+  var_session <- sigma_session/vary
+  var_scanner_session <- sigma_scanner_session/vary
+  var_subid_scanner <- sigma_subid_scanner/vary
+  var_subid_session <- sigma_subid_session/vary
+  varw <- sigma_error/vary
+  #calculate icc
+  icc <- sigma_subid/sigma_all
+  icc_session <- sigma_subid/(sigma_subid+sigma_session+sigma_subid_session+sigma_error)
+  icc_scanner <- sigma_subid/(sigma_subid+sigma_scanner+sigma_subid_scanner+sigma_error)
+  
+  
+  #assign values
+  iccs[,metric] <- c(icc_session,icc_scanner,icc,vary,var_subid,var_scanner,var_session,
+                     var_scanner_session,var_subid_scanner,var_subid_session,varw)
+  
+  p_values[,metric] <- p_value
+  #boot strap
+  boot.icc_sessions = bootMer(fm,calc.icc_session,1000)
+  ci <- quantile(boot.icc_sessions$t,c(0.025,0.975))
+  ci_2.5['icc_session',metric] <- ci[["2.5%"]]
+  ci_97.5['icc_session',metric] <- ci[["97.5%"]]
+  
+  boot.icc_scanner = bootMer(fm,calc.icc_scanner,1000)
+  ci <- quantile(boot.icc_scanner$t,c(0.025,0.975))
+  ci_2.5['icc_scanner',metric] <- ci[["2.5%"]]
+  ci_97.5['icc_scanner',metric] <- ci[["97.5%"]]
+  
+  boot.icc = bootMer(fm,calc.icc,1000)
+  ci <- quantile(boot.icc$t,c(0.025,0.975))
+  ci_2.5['icc',metric] <- ci[["2.5%"]]
+  ci_97.5['icc',metric] <- ci[["97.5%"]]
+  },silent=FALSE)
+}
+
+#check output dir
+output_dir <- paste0(workdir,'/results/','reliability/')
+ifelse(dir.exists(output_dir),print("Saving results into reliability folder"),dir.create(output_dir))
+
+#save results
+fout <- paste0(output_dir,'volbrain_icc.csv')
+write.csv(iccs,fout)
+
+fout <- paste0(output_dir,'volbrain_pvalue.csv')
+write.csv(p_values,fout)
+
+fout <- paste0(output_dir,'volbrain_icc_ci25.csv')
+write.csv(ci_2.5,fout)
+
+fout <- paste0(output_dir,'volbrain_icc_ci975.csv')
+write.csv(ci_97.5,fout)
+
+
+#######################################cal AI icc############################################################################
+#calculat AI
+cal_AI <- function(L,R){((L-R)/(L+R))*2} #define lr asymmetry function
+## right hemi
+df_names <- colnames(volbrain_df)
+R_volume_names <- regexpr('.R.[A-Z][A-Z].cm3|.Right.cm3',df_names) #find metric loc have Right hemi volumes name 
+R_volume_names_index <- ifelse(R_volume_names==-1,FALSE,TRUE) #
+R_df_names <- df_names[R_volume_names_index]
+R_volume_str_end <- R_volume_names[R_volume_names >0] - 1
+struct_names <- substr(R_df_names,1,R_volume_str_end) #get structure names
+struct_names[c(4,5,7,8)] <- c('Cerebelum.GM','Cerebelum.WM','Cerebrum.GM','Cerebrum.WM') #manually edit
+## left hemi 
+L_volume_names <- regexpr('.L.[A-Z][A-Z].cm3|.Left.cm3',df_names) #find metric loc have left hemi volumes name 
+L_volume_names_index <- ifelse(L_volume_names==-1,FALSE,TRUE) #
+L_df_names <- df_names[L_volume_names_index]
+
+nmeasures <- length(L_df_names)
+#setup dataframe for AI analysis
+rownames <- c('icc_session','icc_scanner','icc','vary','var_subid','var_scanner','var_session','var_scanner_session','var_subid_scanner','var_subid_session','varw')
+nrows <- length(rownames)
+iccs <- data.frame(matrix(0,nrows,nmeasures))
+
+names(iccs) <- struct_names
+row.names(iccs) <- rownames
+p_values <- data.frame(matrix(0,2,nmeasures))
+names(p_values) <- struct_names
+row.names(p_values) <- c('Intercept','pvalue')
+ci_2.5 <- data.frame(matrix(0,3,nmeasures))
+names(ci_2.5) <- struct_names
+row.names(ci_2.5) <- c('icc_session','icc_scanner','icc')
+ci_97.5 <- data.frame(matrix(0,3,nmeasures))
+names(ci_97.5) <- struct_names
+row.names(ci_97.5) <- c('icc_session','icc_scanner','icc')
+
+for (i in 1:nmeasures){
+  tmpdf <- volbrain_df
+  L <- volbrain_df[[L_df_names[i]]]
+  R <- volbrain_df[[R_df_names[i]]]
+  y <- cal_AI(L,R)
+  print(struct_names[i])
+  metric <- struct_names[i]
+  index_outliers <- !y %in% boxplot.stats(y)$out  #exclude outliers
+  tmpdf$y <- y
+  tmpdf <- tmpdf[index_outliers,]
+  try({
+    fm <- lmer(y~cage + sex + cheight + cweight + ceducation + cinterval_baseline + (1|subid)+(1|session)+(1|scanner)+(1|subid:session)+(1|subid:scanner)+(1|session:scanner),data=tmpdf)
+    results <- summary(fm)
+    varcor <- as.data.frame(results$varcor)
+    p_value <- results$coefficients[1,c(1,5)]
+    #variance of each factor
+    sigma_subid <- varcor[3,4]
+    sigma_scanner <- varcor[5,4]
+    sigma_session <- varcor[6,4]
+    sigma_scanner_session <-varcor[4,4]
+    sigma_subid_scanner <- varcor[1,4]
+    sigma_subid_session <- varcor[2,4]
+    sigma_error <- varcor[7,4]
+    sigma_all <- sum(varcor$vcov)
+    vary <- var(tmpdf$y)
+    
+    var_subid <- sigma_subid/vary
+    var_scanner <- sigma_scanner/vary
+    var_session <- sigma_session/vary
+    var_scanner_session <- sigma_scanner_session/vary
+    var_subid_scanner <- sigma_subid_scanner/vary
+    var_subid_session <- sigma_subid_session/vary
+    varw <- sigma_error/vary
+    #calculate icc
+    icc <- sigma_subid/sigma_all
+    icc_session <- sigma_subid/(sigma_subid+sigma_session+sigma_subid_session+sigma_error)
+    icc_scanner <- sigma_subid/(sigma_subid+sigma_scanner+sigma_subid_scanner+sigma_error)
+    
+    
+    #assign values
+    iccs[,metric] <- c(icc_session,icc_scanner,icc,vary,var_subid,var_scanner,var_session,
+                       var_scanner_session,var_subid_scanner,var_subid_session,varw)
+    
+    p_values[,metric] <- p_value
+    #boot strap
+    boot.icc_sessions = bootMer(fm,calc.icc_session,1000)
+    ci <- quantile(boot.icc_sessions$t,c(0.025,0.975))
+    ci_2.5['icc_session',metric] <- ci[["2.5%"]]
+    ci_97.5['icc_session',metric] <- ci[["97.5%"]]
+    
+    boot.icc_scanner = bootMer(fm,calc.icc_scanner,1000)
+    ci <- quantile(boot.icc_scanner$t,c(0.025,0.975))
+    ci_2.5['icc_scanner',metric] <- ci[["2.5%"]]
+    ci_97.5['icc_scanner',metric] <- ci[["97.5%"]]
+    
+    boot.icc = bootMer(fm,calc.icc,1000)
+    ci <- quantile(boot.icc$t,c(0.025,0.975))
+    ci_2.5['icc',metric] <- ci[["2.5%"]]
+    ci_97.5['icc',metric] <- ci[["97.5%"]]
+    
+  },silent=FALSE)
+}
+
+#save results
+fout <- paste0(output_dir,'volbrain_icc_subcortical_AI.csv')
+write.csv(iccs,fout)
+
+fout <- paste0(output_dir,'volbrain_pvalue_subcortical_AI.csv')
+write.csv(p_values,fout)
+
+fout <- paste0(output_dir,'volbrain_icc_ci25_subcortical_AI.csv')
+write.csv(ci_2.5,fout)
+
+fout <- paste0(output_dir,'volbrain_icc_ci975_subcortical_AI.csv')
+write.csv(ci_97.5,fout)
+
+print("FINISH ALL")
---
+++ 3R-BRAIN/analyst_wys/code/calc_volbrain_reproducibility.R
@@ -0,0 +1,99 @@
+rm(list=ls())
+workdir <- '~/R3BRAIN_for_OHBM/'  #change it to your data path accordingly
+setwd(workdir)
+library("dplyr")
+library("effsize")
+library("pwr")
+#load 3T reliability results
+data_csv <- paste0(workdir,'data/','volbrain_replicability_3T.csv')
+data_df <- read.csv(data_csv)
+volbrain_ipcas <- data_df %>% filter(scanner=='IPCAS_MR750')
+#volbrain_ipcas <- data_df[which(data_df$scanner=='IPCAS_MR750'),]
+
+
+# orginaze dataframe
+## right hemi
+df_names <- colnames(volbrain_ipcas)
+R_volume_names <- regexpr('.R.[A-Z][A-Z].cm3|.Right.cm3',df_names) #find metric loc have Right hemi volumes name 
+R_volume_names_index <- ifelse(R_volume_names==-1,FALSE,TRUE) #
+R_df_names <- df_names[R_volume_names_index]
+R_volume_str_end <- R_volume_names[R_volume_names >0] - 1
+struct_names <- substr(R_df_names,1,R_volume_str_end) #get structure names
+struct_names[c(4,5,7,8)] <- c('Cerebelum.GM','Cerebelum.WM','Cerebrum.GM','Cerebrum.WM') #manually edit
+## left hemi 
+L_volume_names <- regexpr('.L.[A-Z][A-Z].cm3|.Left.cm3',df_names) #find metric loc have left hemi volumes name 
+L_volume_names_index <- ifelse(L_volume_names==-1,FALSE,TRUE) #
+L_df_names <- df_names[L_volume_names_index]
+
+nmeasures <- length(L_df_names)
+
+#centralize data
+center_cols_names <- c("age","height","weight","education")
+chosen_cols <- volbrain_ipcas[,center_cols_names]
+centered_values <- scale(chosen_cols,center = TRUE)
+centered_cols_names <- c("cage","cheight","cweight","ceducation")
+volbrain_ipcas[,centered_cols_names] <- centered_values
+
+#data_frame_values
+rownames <- c('t','pvalue','mean_diff','lower_bound','upper_bound','effsize','eff_low','eff_up','pwr')
+df_lr <- data.frame(matrix(0,nrow = length(rownames),ncol = nmeasures ),row.names = rownames)
+colnames(df_lr) <- struct_names
+for (i in 1:nmeasures){
+  print(struct_names[[i]])
+  L_metric <- volbrain_ipcas[,L_df_names[i]]
+  R_metric <- volbrain_ipcas[,R_df_names[i]]
+  #paired t-test
+  result <- t.test(L_metric,R_metric,paired = T)
+  effsize <- cohen.d(L_metric,R_metric,paired=T)
+  #cohen's d and conf.int
+  cohend <- effsize$estimate
+  cohend_conf.int <- effsize$conf.int
+  nsubj <- result$n
+  stpwoer <- pwr.t.test(n=length(L_metric),d=cohend,sig.level=0.05,power = NULL,type = "paired",alternative = "two.sided")
+  df_lr[,i] <- c(result[['statistic']],result[['p.value']],result[['estimate']],result[["conf.int"]][1],result[["conf.int"]][2],cohend,cohend_conf.int[1],cohend_conf.int[2],stpwoer$power)
+}
+fout <- paste0(workdir,'results/','reproducibility/','volbrain_ipcas_for-3T_LR_diff_by_wys.csv')
+write.csv(df_lr,fout)
+
+
+####################### gender differences########################################
+#calculat AI
+cal_AI <- function(L,R){((L-R)/(L+R))*2} #define lr asymmetry function
+
+volbrain_ipcas$gender <- as.factor(volbrain_ipcas$gender)
+ICV <- volbrain_ipcas$Tissue.IC.cm3
+volbrain_ipcas$cICV <- scale(ICV,center=T)
+
+#cal gender and age diff
+gender_distribute_diff <- chisq.test(summary(volbrain_ipcas$gender))
+volbrain_ipcas_male <- volbrain_ipcas %>% filter(gender=='1')
+volbrain_ipcas_female <- volbrain_ipcas %>% filter(gender=='0')
+age_diff <- t.test(volbrain_ipcas_male$age,volbrain_ipcas_female$age)
+education_diff <- t.test(volbrain_ipcas_male$education,volbrain_ipcas_female$education)
+#save gender age education p
+matched_test <- data.frame(gender_chisq_p=gender_distribute_diff$p.value,age_diff_p = age_diff$p.value,education_diff_p=education_diff$p.value)
+fout <-  paste0(workdir,'results/','reproducibility/','gender_match_test.csv')
+write.csv(matched_test,fout)
+
+#loop each metric calculate the residual p value after regressed age and icv
+idx_male <- which(volbrain_ipcas['gender']=='1')
+idx_female <- which(volbrain_ipcas['gender']=='0')
+rownames <- c('t','pvalue','mean_diff_male','mean_diff_female','lower_bound','upper_bound','effsize','eff_low','eff_up','pwr')
+df_gender_diff <- data.frame(matrix(0,nrow = length(rownames),ncol = nmeasures ),row.names = rownames)
+colnames(df_gender_diff) <- struct_names
+
+for (k in 1:nmeasures){
+  print(struct_names[k])
+  metric_AI <- cal_AI(volbrain_ipcas[,L_df_names[k]],volbrain_ipcas[,R_df_names[k]])
+  tmp_df <- data.frame(metric_AI=metric_AI,cICV=volbrain_ipcas$cICV)
+  residuals <- lm(metric_AI ~ cICV,data = tmp_df) %>% residuals  #regress icv
+  result <- t.test(residuals[idx_male],residuals[idx_female])
+  effsize <- cohen.d(residuals[idx_male],residuals[idx_female])
+  #cohen's d and conf.int
+  cohend <- effsize$estimate
+  cohend_conf.int <- effsize$conf.int
+  stpwoer <- pwr.t.test(n=length(residuals),d=cohend,sig.level=0.05,power = NULL,type = "two.sample",alternative = "two.sided")
+  df_gender_diff[,k] <- c(result[['statistic']],result[['p.value']],result[['estimate']][1],result[['estimate']][2],result[["conf.int"]][1],result[["conf.int"]][2],cohend,cohend_conf.int[1],cohend_conf.int[2],stpwoer$power)
+}
+fout <- paste0(workdir,'results/','reproducibility/','volbrain_ipcas_for-3T_gender-diff_by_wys.csv')
+write.csv(df_gender_diff,fout)
